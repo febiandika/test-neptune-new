1e0538dfbf3aef9054bbf73225d4343fdea1296f
Febi Andika <48900460+febiandika@users.noreply.github.com>
2021-10-05 14:50:46 +0700

    Update train.py

--- train.py
+++ train.py
@@ -69,3 +69,4 @@ neptune.log_image('performance charts', fig_pr)
 # Handle CI pipeline details
 if os.getenv('CI') == "true":
     neptune.append_tag('ci-pipeline', os.getenv('NEPTUNE_EXPERIMENT_TAG_ID'))
+#
